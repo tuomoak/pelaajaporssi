73a2701e29fc53034bed05c9f1b3b7037eb2359a
Tuomo Ala-Kojola <tuomo.alakojola@gmail.com>
2025-11-16 17:33:14 +0200

    Feature: Adding players to database

--- app.py
+++ app.py
@@ -1,7 +1,62 @@
 from flask import Flask
+from flask import redirect, render_template, request
+import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Tähän tulee pelaajapörssi"
+    message = "Pelaajapörssi on pesäpalloaiheinen sivusto, joka on tarkoitettu seuraaville:"
+    words = ["Joukkuetta etsiville", "Pelaajia etsiville"]
+    return render_template("index.html", message=message, items=words)
+
+@app.route("/add_player")
+def add_player():
+    return render_template("add_player.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    firstname = request.form["firstname"]
+    surname = request.form["surname"]
+    outfield_positions = request.form.getlist("outfield_position")
+    infield_roles = request.form.getlist("infield_role")
+    profile = request.form["profile"]
+
+    name=firstname + str(" ") + surname
+  
+    columns = "NAME, PROFILE"
+    column_places = "?,?"
+    
+    values = [name, profile]
+
+    for of_position in outfield_positions:
+       columns += ", " + str(of_position)
+       column_places +=  ",?" 
+       values.append(1)
+      
+    for infield_role in infield_roles:
+       columns += ", " + str(infield_role)
+       column_places +=  ",?"
+       values.append(1)
+
+    db.execute(f"INSERT INTO players ({columns}) VALUES ({column_places})",values)
+        
+    return render_template("result.html", name=firstname + str(" ") + surname, outfield_positions=outfield_positions, infield_roles=infield_roles, profile=profile)
+
+@app.route("/teams")
+def teams():
+    return "Tähän tulee palvelussa olevat joukkueet."
+
+@app.route("/players")
+def players():
+    all_players = db.query("SELECT * FROM players")
+    count = len(all_players)
+    return render_template("/players.html", count=count, all_players=all_players)
+
+@app.route("/player/<int:page_id>")
+def player(page_id):
+    return "Tämä pelaajan nro sivu: " + str(page_id)
+
+@app.route("/team/<int:page_id>")
+def team(page_id):
+    return "Tämä on joukkueen nro sivu: " + str(page_id)
---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE players (
+    id INTEGER PRIMARY KEY,
+    NAME TEXT,
+    PROFILE TEXT,
+    lukkari BIT  DEFAULT 0 NOT NULL,
+    etukenttä BIT  DEFAULT 0 NOT NULL,
+    polttolinja BIT  DEFAULT 0 NOT NULL,
+    pesävahti BIT  DEFAULT 0 NOT NULL,   
+    takakenttä BIT  DEFAULT 0 NOT NULL,  
+    etenijä BIT  DEFAULT 0 NOT NULL,    
+    vaihtaja BIT  DEFAULT 0 NOT NULL,  
+    kotiuttaja BIT  DEFAULT 0 NOT NULL,  
+    kopittaja BIT  DEFAULT 0 NOT NULL 
+    );           
+ 
